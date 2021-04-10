12e03dcfc2356547a3693f1ed4641dd0e322d896
Josh Davies <josh@joshdavies.tech>
2021-04-10 23:08:20 +0100

    Remove deploy.sh
    Was only used by Azure so can be removed safely (thanks Oscar!)

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-SB_ROOT=/fac/sci/dcs/teaching/material/cs141
-
-chmod +x $SB_UTIL
-
-$SB_UTIL edit --page=$SB_ROOT --file=material.html
-$SB_UTIL edit --page=$SB_ROOT/term3 --file=term3.html
-$SB_UTIL edit --page=$SB_ROOT/online-labs --file=online-labs.html
